14a8fd1b089b988ef1efc9ca6af892275bddb3eb
Binath Hettiarachhci <hdbinath@gmail.com>
2024-09-10 00:49:40 +0530

    Tp operator

--- telopr/get_driver_details.php
+++ telopr/get_driver_details.php
@@ -1,21 +1,43 @@
 <?php
 include('vendor/inc/config.php');
 
-if(isset($_POST['v_reg_no'])) {
-    $v_reg_no = $_POST['v_reg_no'];
+$v_id = isset($_POST['v_id']) ? $_POST['v_id'] : '';
+$v_category = isset($_POST['v_category']) ? $_POST['v_category'] : '';
 
-    $query = "SELECT v_driver, v_driver_contact FROM tms_vehicle WHERE v_reg_no = ?";
-    $stmt = $mysqli->prepare($query);
-    $stmt->bind_param('s', $v_reg_no);
-    $stmt->execute();
-    $stmt->bind_result($v_driver, $v_driver_contact);
-    $stmt->fetch();
+// First, get the vehicle details from tms_vehicle table
+$vehicle_query = "SELECT v_id, v_reg_no, v_category, d_id FROM tms_vehicle WHERE v_id = ? OR v_category = ?";
+$vehicle_stmt = $mysqli->prepare($vehicle_query);
+$vehicle_stmt->bind_param('is', $v_id, $v_category);
+$vehicle_stmt->execute();
+$vehicle_result = $vehicle_stmt->get_result();
+$vehicle_row = $vehicle_result->fetch_assoc();
 
-    $driver_details = array(
-        'v_driver' => $v_driver,
-        'v_driver_contact' => $v_driver_contact
-    );
+if ($vehicle_row) {
+    $d_id = $vehicle_row['d_id'];
+    $v_reg_no = $vehicle_row['v_reg_no'];
+    $v_category = $vehicle_row['v_category'];
 
-    echo json_encode($driver_details);
+    // Now, get the driver details from tms_driver table
+    $driver_query = "SELECT d_fname, d_lname, d_phone FROM tms_driver WHERE d_id = ?";
+    $driver_stmt = $mysqli->prepare($driver_query);
+    $driver_stmt->bind_param('i', $d_id);
+    $driver_stmt->execute();
+    $driver_result = $driver_stmt->get_result();
+    $driver_row = $driver_result->fetch_assoc();
+
+    if ($driver_row) {
+        $driver_details = array(
+            'driver_name' => $driver_row['d_fname'] . ' ' . $driver_row['d_lname'],
+            'driver_contact' => $driver_row['d_phone'],
+            'd_id' => $d_id,
+            'v_reg_no' => $v_reg_no,
+            'v_category' => $v_category
+        );
+        echo json_encode($driver_details);
+    } else {
+        echo json_encode(array('error' => 'No driver found for this vehicle.'));
+    }
+} else {
+    echo json_encode(array('error' => 'No vehicle found with the given details.'));
 }
 ?>
