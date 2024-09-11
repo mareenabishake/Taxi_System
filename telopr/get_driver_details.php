0ad8100a465707dc63855f5d7e058f666055c2c5
Mareen Abishake <mareen.abishake@gmail.com>
2024-09-11 11:04:09 +0530

    Operator Booking

--- telopr/get_driver_details.php
+++ telopr/get_driver_details.php
@@ -1,43 +1,34 @@
 <?php
 include('vendor/inc/config.php');
 
-$v_id = isset($_POST['v_id']) ? $_POST['v_id'] : '';
-$v_category = isset($_POST['v_category']) ? $_POST['v_category'] : '';
-
-// First, get the vehicle details from tms_vehicle table
-$vehicle_query = "SELECT v_id, v_reg_no, v_category, d_id FROM tms_vehicle WHERE v_id = ? OR v_category = ?";
-$vehicle_stmt = $mysqli->prepare($vehicle_query);
-$vehicle_stmt->bind_param('is', $v_id, $v_category);
-$vehicle_stmt->execute();
-$vehicle_result = $vehicle_stmt->get_result();
-$vehicle_row = $vehicle_result->fetch_assoc();
-
-if ($vehicle_row) {
-    $d_id = $vehicle_row['d_id'];
-    $v_reg_no = $vehicle_row['v_reg_no'];
-    $v_category = $vehicle_row['v_category'];
-
-    // Now, get the driver details from tms_driver table
-    $driver_query = "SELECT d_fname, d_lname, d_phone FROM tms_driver WHERE d_id = ?";
-    $driver_stmt = $mysqli->prepare($driver_query);
-    $driver_stmt->bind_param('i', $d_id);
-    $driver_stmt->execute();
-    $driver_result = $driver_stmt->get_result();
-    $driver_row = $driver_result->fetch_assoc();
-
-    if ($driver_row) {
-        $driver_details = array(
-            'driver_name' => $driver_row['d_fname'] . ' ' . $driver_row['d_lname'],
-            'driver_contact' => $driver_row['d_phone'],
-            'd_id' => $d_id,
-            'v_reg_no' => $v_reg_no,
-            'v_category' => $v_category
+if(isset($_POST['v_id'])) {
+    $v_id = $_POST['v_id'];
+    
+    $query = "SELECT v.v_cost, v.d_id, CONCAT(d.d_fname, ' ', d.d_lname) AS driver_name 
+              FROM tms_vehicle v 
+              LEFT JOIN tms_driver d ON v.d_id = d.d_id 
+              WHERE v.v_id = ?";
+    
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param('i', $v_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if($row = $result->fetch_assoc()) {
+        $response = array(
+            'driver_name' => $row['driver_name'],
+            'd_id' => $row['d_id'],
+            'v_cost' => $row['v_cost']
         );
-        echo json_encode($driver_details);
+        echo json_encode($response);
     } else {
-        echo json_encode(array('error' => 'No driver found for this vehicle.'));
+        echo json_encode(array('error' => 'Vehicle or driver not found'));
     }
+    
+    $stmt->close();
 } else {
-    echo json_encode(array('error' => 'No vehicle found with the given details.'));
+    echo json_encode(array('error' => 'No vehicle ID provided'));
 }
+
+$mysqli->close();
 ?>
--- telopr/get_vehicle_regno.php
+++ telopr/get_vehicle_regno.php
@@ -3,21 +3,15 @@ include('vendor/inc/config.php');
 
 if(isset($_POST['v_category'])) {
     $v_category = $_POST['v_category'];
-    
     $query = "SELECT v_id, v_reg_no FROM tms_vehicle WHERE v_category = ? AND v_status = 'Available'";
     $stmt = $mysqli->prepare($query);
     $stmt->bind_param('s', $v_category);
     $stmt->execute();
     $result = $stmt->get_result();
-    
-    $output = '<option value="">Select Vehicle Registration</option>';
-    while($row = $result->fetch_assoc()) {
-        $output .= '<option value="'.$row['v_id'].'">'.$row['v_reg_no'].'</option>';
+
+    echo '<option value="">Select Vehicle Registration</option>';
+    while ($row = $result->fetch_assoc()) {
+        echo '<option value="' . $row['v_id'] . '">' . $row['v_reg_no'] . '</option>';
     }
-    
-    echo $output;
-    
-    $stmt->close();
 }
-$mysqli->close();
 ?>
